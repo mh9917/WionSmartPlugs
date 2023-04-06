1ec6ac5527e21a643390f02657bad7693ef43763
mh9917 <91633359+mh9917@users.noreply.github.com>
2023-04-06 19:51:56 -0400

    Add files via upload

---
+++ manifest.py
@@ -0,0 +1,13 @@
+{
+  "domain": "wion",
+  "name": "Wion Smart Plugs",
+  
+  "dependencies": [
+    "homeassistant"
+  ],
+  "codeowners": [],
+  "requirements": [
+    "pywion==0.0.1"
+  ],
+  "version": "1.0.0"
+}
